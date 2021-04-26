8767d012292353087e22792450683fa29db7be67
Amir Shams <amirshams@Amirs-MacBook-Pro.local>
2021-04-26 16:32:51 +0430

    Updates

---
+++ precache-manifest.421dffbadf17403fb9ac984314f66eb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "461068b8e4a49eb387bc",
+    "url": "/todo/static/css/main.b5810c50.chunk.css"
+  },
+  {
+    "revision": "461068b8e4a49eb387bc",
+    "url": "/todo/static/js/main.461068b8.chunk.js"
+  },
+  {
+    "revision": "b211a975d00ec5f50984",
+    "url": "/todo/static/js/1.b211a975.chunk.js"
+  },
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "ddd003fad62b25de44e8dab8d2288d34",
+    "url": "/todo/index.html"
+  }
+];
